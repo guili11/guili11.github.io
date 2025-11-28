dc8be23019a792967f22742292ee9dd3d88d955b
guili11 <2654044140@qq.com>
2025-11-28 21:52:51 +0800

    提交格式化代码

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
-<script setup lang="ts"></script>
-
 <template>
   <div>
-    <h2>this is app vue</h2>
+
   </div>
 </template>
 
+<script setup lang="ts">
+for(let i=0;i<100;i++){
+  console.log(i);
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
